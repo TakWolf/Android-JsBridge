ed927b13178d6101094e92e98fb394b0a5f4d9b0
TakWolf <takwolf@foxmail.com>
2023-09-30 05:31:57 +0800

    0.0.3

--- jsbridge/build.gradle.kts
+++ jsbridge/build.gradle.kts
@@ -39,7 +39,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.takwolf.android.jsbridge"
             artifactId = "jsbridge"
-            version = "0.0.2"
+            version = "0.0.3"
 
             afterEvaluate {
                 from(components["release"])
