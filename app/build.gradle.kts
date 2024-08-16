29f9c62918c10c5725b521c215f2b299b2c07eb7
TakWolf <takwolf@foxmail.com>
2024-08-16 16:43:17 +0800

    调整样式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,5 +39,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("com.github.TakWolf.Android-InsetsWidget:insetswidget:0.0.1")
     implementation(project(":jsbridge"))
 }
--- app/src/main/java/com/takwolf/android/demo/jsbridge/MainActivity.kt
+++ app/src/main/java/com/takwolf/android/demo/jsbridge/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.webkit.WebView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.takwolf.android.demo.jsbridge.databinding.ActivityMainBinding
 import com.takwolf.android.jsbridge.registerBridgeService
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
